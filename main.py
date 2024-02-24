0b1e79f9b9551e771fb57e0f50ce74ff7a4cf9a2
programist_sasha <irov-sasha1191@yandex.ru>
2024-02-24 14:15:03 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,9 @@
+from PyQt6.QtWidgets import QApplication,QWidget,QLabel,QVBoxLayout,QPushButton,QRadioButton,QSlider,QDial,QLineEdit,QGroupBox, QHBoxLayout
+from PyQt6.QtCore import Qt
+from random import randint
+from  random import shuffle
+app = QApplication([])
+window = QWidget()
+window.show()
+
+app.exec()
