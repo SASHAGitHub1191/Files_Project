5b9e21836b3bd998a6fab7ddc4c7e51f94ac77de
User <irov-sasha1191@yandex.ru>
2024-02-25 15:00:06 +0300

    ggg

--- main.py
+++ main.py
@@ -1,9 +1,18 @@
-from PyQt6.QtWidgets import QApplication,QWidget,QLabel,QVBoxLayout,QPushButton,QRadioButton,QSlider,QDial,QLineEdit,QGroupBox, QHBoxLayout
+from PyQt6.QtWidgets import QApplication,QWidget, QTextEdit,QListWidget, QLabel,QVBoxLayout,QPushButton,QHBoxLayout, QRadioButton,QSlider,QDial,QLineEdit,QGroupBox, QHBoxLayout
+
 from PyQt6.QtCore import Qt
 from random import randint
 from  random import shuffle
 app = QApplication([])
 window = QWidget()
 window.show()
+h_line = QHBoxLayout()
+v_line = QVBoxLayout()
+text = QTextEdit()
+qlist = QListWidget()
+window.setLayout(h_line)
+h_line.addWidget(text)
+h_line.addLayout(v_line)
+v_line.addWidget(qlist)
 
 app.exec()
